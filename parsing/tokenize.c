66111668f516c9f137ea5a5d63547544b2ee889c
Lottfi433 <74673953+Lottfi433@users.noreply.github.com>
2025-04-27 10:32:33 +0100

    Update tokenize.c

---
+++ parsing/tokenize.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yazlaigi <yazlaigi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 11:53:21 by yazlaigi          #+#    #+#             */
+/*   Updated: 2025/04/26 13:39:34 by yazlaigi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		i;
+
+	if (!s1)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+token_type	tokenize_type(char *input, int *i)
+{
+	if (input[*i] == '>' && input[*i + 1] == '>')
+	{
+		(*i)++;
+		return (REDIR_APPEND);
+	}
+	else if (input[*i] == '|')
+		return (PIPE);
+	else if (input[*i] == '<')
+		return (REDIR_IN);
+	else if (input[*i] == '>')
+		return (REDIR_OUT);
+}
+
+token	*token_creation(char *value, token_type type)
+{
+	token	*new_token;
+	
+	new_token = malloc (sizeof(token));
+	if (!new_token)
+		return (NULL);
+	new_token->value = value;
+	new_token->type = type;
+	new_token->next = NULL;
+}
+
+void	token_add_back(token **head, token *new_token)
+{
+	token	*tmp;
+	if (!*head)
+		*head = new_token;
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_token;
+	}
+}
+token	*tokenize(char *input)
+{
+	int			i;
+	token		*head;
+	token		*current;
+	char		*token_value;
+	token_type	type;
+	int			start;
+	i = 0;
+	head = NULL;
+	while (input[i] != '\0')
+	{
+		if (input[i] == 32 || (input[i] >= 9 && input[i] <= 13))
+			i++;
+		if (input[i] == '|' || input[i] == '>' || input[i] == '<')
+		{
+			type = tokenize_type(input, &i);
+			token_value = malloc (3);
+			token_value[0] = input [i];
+			if (type == REDIR_APPEND)
+				token_value[1] = input[i + 1];
+			token_value[2] = '\0';
+			current = token_creation(token_value, type);
+			token_add_back(&head, current);
+		i++;
+		}
+		else
+		{
+			start = i;
+			while (input[i] && !(input[i] == ' ' || input[i] == '\t' || input[i] == '\n'
+				|| input[i] == '|' || input[i] == '<' || input[i] == '>'))
+				i++;
+			token_value = ft_strndup(&input[start], i - start);
+			current = create_token(token_value, WORD);
+			add_token_back(&head, current);
+		}
+	}
+}
