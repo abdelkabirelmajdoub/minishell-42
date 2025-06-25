13ec0720da6e95a0bc6b73fee7f8ec93c54a9d28
Abdelkabir El Majdoub <ael-majd@c2r5p10.1337.ma>
2025-06-25 10:43:03 +0100

    some leaks

--- parsing/tokenize_pars2.c
+++ parsing/tokenize_pars2.c
@@ -6,7 +6,7 @@
 /*   By: ael-majd <ael-majd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 13:51:39 by yazlaigi          #+#    #+#             */
-/*   Updated: 2025/06/23 17:51:34 by ael-majd         ###   ########.fr       */
+/*   Updated: 2025/06/25 10:39:48 by ael-majd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,9 @@ void	pars_helper2(t_token **tok, t_cmd *cmd, int *i)
 	else if ((*tok)->type == REDIR_OUT)
 		cmd->out_file[(*i)++] = ft_strdup((*tok)->next->value);
 	else if ((*tok)->type == REDIR_HEREDOC)
+	{
+		if (cmd->limiter)
+			free(cmd->limiter);
 		cmd->limiter = ft_strdup((*tok)->next->value);
+	}
 }
